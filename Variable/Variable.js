c19c9114fd4588fca4fd0b971e4e484e8321738f
Echo <Echoece@gmail.com>
2020-12-13 21:04:20 +0600

    Initial commit

---
+++ Variable/Variable.js
@@ -0,0 +1,9 @@
+'use strict'
+// let and var are mutable variable. difference is var is global scope and let is block scoped.
+// let is the preferred way now.
+let x = 10;
+
+// we must initialize const variables, otherwise it will give an error.
+// const are immutable variables.
+const y=100;
+
